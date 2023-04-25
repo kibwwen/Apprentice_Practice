f5a8abf2cd14578dc26c0cdc5501ccecf61e5d99
kibwwen <kibwwen@gmail.com>
2023-04-25 09:00:52 +0000

    password manager finish

--- Quest2/password.sh
+++ Quest2/password.sh
@@ -4,20 +4,28 @@ do
   read -p "次の選択肢から入力してください([A]Add Password/[G]Get Password/[E]Exit):" select
   if [ "$select" = "A" ]; then
     read -p "サービス名を入力してください：" service
-    echo "サービス名:$service" >> password.txt
     read -p "ユーザー名を入力してください：" user
-    echo "ユーザー名:$user" >> password.txt
     read -p "パスワードを入力してください：" pass
+    gpg password.txt.gpg 2> /dev/null
+    rm ./password.txt.gpg
+    echo "サービス名:$service" >> password.txt
+    echo "ユーザー名:$user" >> password.txt
     echo "パスワード:$pass" >> password.txt
+    gpg -e -r kibwwen@gmail.com password.txt
+    rm ./password.txt
     echo "Thank you!"
 
   elif [ "$select" = "G" ]; then
     file=./password.txt
     read -p "サービス名を入力してください：" service
-    test=$(grep -A 2 "$service" "$file")
-    if [ "$test" != "" ];
+    gpg password.txt.gpg 2> /dev/null
+    rm ./password.txt.gpg
+    value=$(grep -A 2 "$service" "$file")
+    gpg -e -r kibwwen@gmail.com password.txt
+    rm ./password.txt
+    if [ "$value" != "" ];
     then
-      echo "$test"
+      echo "$value"
     else
       echo "そのサービスは登録されていません"
     fi
