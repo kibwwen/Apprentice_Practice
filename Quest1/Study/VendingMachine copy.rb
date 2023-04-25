fb25a13573a9de169ce73da4c14df02a2f81c04e
kibwwen <kibwwen@gmail.com>
2023-04-25 09:52:57 +0000

    file test

--- Quest1/Study/VendingMachine copy.rb
+++ Quest1/Study/VendingMachine copy.rb
@@ -1,39 +1,42 @@
-# frozen_string_literal: true
-# テスト
 class VendingMachine
+
   def initialize(maker)
     @maker = maker
     @coin = 0
   end
 
   def deposit_coin(coin)
-    return unless coin
-
-    @coin += coin
-    puts "#{@coin}円です"
+    if coin
+      @coin += coin
+      puts "#{@coin}円です"
+    end
   end
-
+  
   def press_button
+
     puts 'sider'
   end
 
   def press_manufacturer_name
-    puts @maker
+      puts @maker
   end
 
   private :press_manufacturer_name
+
 end
 
-# マシン
 class MerchandiseType
-  def merchandise_type; end
+  
+  def merchandise_type
+
+  end
 end
 
 vending_machine = VendingMachine.new('サントリー')
 vending_machine.press_button
-begin
+begin  
   vending_machine.press_manufacturer_name
-rescue StandardError => e
+rescue => e
   puts "#{e.message} (#{e.class})"
 end
 vending_machine.deposit_coin(100)
